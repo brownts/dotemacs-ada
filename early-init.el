562450b26b62dc8a01d5eb9f29f6abf197668492
Troy Brown <brownts@troybrown.dev>
2025-01-04 12:12:05 -0500

    Use different package directory for different Emacs versions.

--- early-init.el
+++ early-init.el
@@ -13,6 +13,15 @@
 
 (setq-default indent-tabs-mode nil)
 
+;; Byte-compiled files may not be compatible between different Emacs
+;; versions, and since (M)ELPA installed packages are compiled in
+;; their own sub-directories within `package-user-dir', use a separate
+;; `package-user-dir' for different versions of Emacs.
+(setq package-user-dir
+      (expand-file-name
+       (format "elpa/%s.%s" emacs-major-version emacs-minor-version)
+       user-emacs-directory))
+
 (provide 'early-init)
 
 ;;; early-init.el ends here
