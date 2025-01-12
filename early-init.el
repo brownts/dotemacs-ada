291d7486ebfbdc3c0f5f08efb600f35448dc136c
Troy Brown <brownts@troybrown.dev>
2025-01-12 15:56:13 -0500

    Don't display the Warnings buffer for native compilation warnings.

--- early-init.el
+++ early-init.el
@@ -13,6 +13,9 @@
 
 (setq-default indent-tabs-mode nil)
 
+;; Ignore native compilation warnings (usually a nuisance)
+(setq native-comp-async-report-warnings-errors 'silent)
+
 ;; Byte-compiled files may not be compatible between different Emacs
 ;; versions, and since (M)ELPA installed packages are compiled in
 ;; their own sub-directories within `package-user-dir', use a separate
